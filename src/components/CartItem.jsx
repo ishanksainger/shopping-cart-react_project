b075b1da5be6023eb299edea55190ab010559377
ishanksainger <isainger29@gmail.com>
2023-09-13 14:27:04 +0530

    push code
    Complete code

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,45 @@
+import {AiFillDelete} from  'react-icons/ai'
+import { useDispatch, useSelector } from 'react-redux';
+import { remove } from '../redux/Slices/CartSlice';
+import { toast } from 'react-hot-toast';
+
+const CartItem = (props) => {
+  const {item,itemIndex,indexItem}=props
+  const {cart}=useSelector((state) => state)
+  const dispatch=useDispatch();
+
+  const removeFromCart=()=>{
+    dispatch(remove(itemIndex));
+    toast.error("Removed from Cart")
+  }
+
+  // Define the CSS class for the border
+  const isNotLastItem = indexItem !== cart.length - 1;
+
+  // Define the CSS class for the border
+  const borderClass = isNotLastItem ? 'border-b-2' : '';
+  
+  return (
+      <div className={`flex items-center p-2 md:p-5 justify-between ${borderClass} border-slate-500  mt-2 mb-2 md:mx-5`}>
+      <div className='flex flex-col md:flex-row p-0 md:p-3 gap-5 items-center'>
+      <div className='w-[30%]'>
+        <img src={item.image} alt="" className=' object-cover'/>
+      </div>
+      <div className='md:ml-10 self-start space-y-5 w-[100%] md:w-[70%]'>
+        <h1 className='text-xl text-slate-700 font-semibold'>{item.title}</h1>
+        <h1 className='text-base text-slate-700 font-medium'>{item.description.split(" ").slice(0,20).join(" ") + "..."}</h1>
+        <div className='flex items-center justify-between'>
+          <p className='font-bold text-lg text-green-600'>${item.price}</p>
+          <div className=' bg-red-200 group hover:bg-red-400 transition-transform duration-300 cursor-pointer rounded-full p-3 mr-3' onClick={removeFromCart}>
+            <AiFillDelete className=' text-red-800'/>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+
+  )
+};
+
+export default CartItem;
